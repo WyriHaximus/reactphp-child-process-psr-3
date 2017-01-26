fed4252610df51767c275094f8d8e97cd5918d8a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-01-26 08:16:12 +0100

    Example logger

---
+++ src/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WyriHaximus\React\ChildProcess\PSR3;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+
+function example_stream_factory(): LoggerInterface
+{
+    return new Logger(
+        'example',
+        [
+            new StreamHandler(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'example.log'),
+        ]
+    );
+}
---
+++ src/functions_include.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WyriHaximus\React\ChildProcess\PSR3;
+
+// @codeCoverageIgnoreStart
+if (!function_exists('WyriHaximus\React\ChildProcess\PSR3\example_stream_factory')) {
+    require __DIR__ . '/functions.php';
+}
+// @codeCoverageIgnoreEnd
